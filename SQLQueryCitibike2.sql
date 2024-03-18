a29c94c26f99d74ff7447993625a8f599bf6c4f8
Arsen <arsenruslanovich789@gmail.com>
2024-03-18 09:13:23 +0100

    Projektdateien hinzufügen.

---
+++ SQLQueryCitibike2.sql
@@ -0,0 +1,15 @@
+﻿
+CREATE VIEW v_StationDetails AS
+SELECT StationId,
+		Name,
+		Lattitude,
+		Longitude
+FROM Citibike2.DM.DIM_Station;
+
+CREATE VIEW v_RiderDetails AS
+SELECT RiderId,
+		Gender,
+		UserType,
+		BirthYear,
+		AgeRange
+FROM Citibike2.DM.DIM_Station;
